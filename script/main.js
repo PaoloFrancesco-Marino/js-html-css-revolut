aa027e21fb2bf331a436674e425ff89805bd30d0
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-14 19:12:33 +0200

    add css style for voice menu (beta inside menu)

--- script/main.js
+++ script/main.js
@@ -2,29 +2,18 @@ $(document).ready(function () {
 
 
     // rendere funzionali i menu dropdown all’interno della navigazione principale
-    
-    var dropDown = $('.right-nav ul .menu-drop');
 
-    var dropLink = dropDown.children('a');
+    // refs 
+    var dropDown = $('.right-nav ul .menu-drop'); //seleziono il padre 
+    var dropLink = dropDown.children('a'); //seleziono le a figlie dirette del padre
+    var dropMenu = dropDown.children('.dropdown-menu'); // seleziono i contenitori con classe dropdown-menu
 
-    var dropMenu = dropDown.children('.dropdown-menu');
-
-
+    // ref icon change
     var arrowChange = $('.right-nav ul li:nth-child(6) a'); //select a link
     var iconChange = $('.right-nav ul li:nth-child(6) a i'); //select icon change
 
 
-    // icon change
-
-    arrowChange.mouseenter(function () { 
-        iconChange.removeClass('fas fa-chevron-down')
-        iconChange.addClass('fas fa-chevron-up')
-    });
-
-    arrowChange.mouseleave(function () { 
-        iconChange.removeClass('fas fa-chevron-up')
-        iconChange.addClass('fas fa-chevron-down')
-    });
+ 
 
     //mouse click
     dropLink.click(function () { 
@@ -35,6 +24,18 @@ $(document).ready(function () {
 
         actualMenu.toggle();
 
+    });
+
+    // icon change
+
+       arrowChange.mouseenter(function () { 
+        iconChange.removeClass('fas fa-chevron-down')
+        iconChange.addClass('fas fa-chevron-up')
+    });
+
+    arrowChange.mouseleave(function () { 
+        iconChange.removeClass('fas fa-chevron-up')
+        iconChange.addClass('fas fa-chevron-down')
     });
 
 
