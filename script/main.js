f35020d70da7995c3481189f2c6e7ebc1ad5b8f9
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-14 17:56:08 +0200

    add js control (to-do mouse-leave alternative, and arrow change navbar)

--- script/main.js
+++ script/main.js
@@ -1,1 +1,50 @@
-console.log('js work');
+$(document).ready(function () {
+
+
+    // rendere funzionali i menu dropdown all’interno della navigazione principale
+    
+    var dropDown = $('.right-nav ul .menu-drop');
+
+    var dropLink = dropDown.children('a');
+
+    var dropMenu = dropDown.children('.dropdown-menu');
+
+
+
+
+    //mouse click
+    dropLink.click(function () { 
+
+        var actualMenu = $(this).next('.dropdown-menu');
+
+        dropMenu.not(actualMenu).hide();
+
+        actualMenu.toggle();
+
+    });
+
+
+    // // alternative mouse enter / mouse leave 
+
+    // dropLink.mouseenter(function () { 
+
+    //     var actualMenu = $(this).next('.dropdown-menu');
+
+    //     dropMenu.not(actualMenu).hide();
+
+    //     actualMenu.toggle();
+
+    // });
+
+    // dropLink.mouseleave(function () { 
+
+    //     var actualMenu = $(this).next('.dropdown-menu');
+
+    //     dropMenu.not(actualMenu).hide();
+
+    //     actualMenu.toggle();
+
+    // });
+
+    
+}); //end document ready
